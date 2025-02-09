d1466ffb7770742cab4968efd5eac1709fa2c599
LOKI-25 <plokeshn25@gmail.com>
2025-02-09 16:51:39 -0600

    changed the domain path

--- src/queries/getDatoCmsToken.ts
+++ src/queries/getDatoCmsToken.ts
@@ -3,13 +3,11 @@
 export const getDatoCmsToken = (): string => {
   const hostname = window.location.hostname;
 
-  switch (hostname) {
-    case 'localhost':
-      return '36f5d415afdc52c14c3f229f8e660c';
-    case 'portfolio-five-henna-63.vercel.app':
-      return '36f5d415afdc52c14c3f229f8e660c';
-
-    default:
-      throw new Error(`No DatoCMS token configured for hostname: ${hostname}`);
+  // Check if hostname is 'localhost' or ends with 'vercel.app'
+  if (hostname === 'localhost' || hostname.endsWith('.vercel.app')) {
+    return '36f5d415afdc52c14c3f229f8e660c';
   }
+
+  // If the hostname doesn't match, throw an error
+  throw new Error(`No DatoCMS token configured for hostname: ${hostname}`);
 };
